36ba40dcf541ab4e4a50c80dcb70b2679e30397c
paul-topshamcom <paul.marquick@gmail.com>
2025-08-31 14:00:38 +0100

    Commented everything out.

--- CountryService.WebApi/Program.cs
+++ CountryService.WebApi/Program.cs
@@ -23,6 +23,7 @@ using Microsoft.Extensions.Logging;
 using OpenTelemetry.Metrics;
 using Scalar.AspNetCore;
 using Serilog;
+using Microsoft.Extensions.Logging.AzureAppServices;
 
 namespace CountryService.WebApi;
 
@@ -32,167 +33,178 @@ internal class Program
     {
         Console.WriteLine("Country Service Web Api start up.");
 
-        const string allowAdminApp = "allowAdminApp";
+    //    const string allowAdminApp = "allowAdminApp";
 
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        builder.Host.UseSerilog((ctx, lc) => lc
-            .WriteTo.Console(outputTemplate: "level={Level:w} {Properties} msg={Message:lj} {NewLine}{Exception}")
-            .Enrich.FromLogContext()
-            .Enrich.WithProperty("Application_name", "CountryService.ApiService")
-            .ReadFrom.Configuration(builder.Configuration)
-        );
+    //    builder.Host.UseSerilog((ctx, lc) => lc
+    //        .WriteTo.Console(outputTemplate: "level={Level:w} {Properties} msg={Message:lj} {NewLine}{Exception}")
+    //        .Enrich.FromLogContext()
+    //        .Enrich.WithProperty("Application_name", "CountryService.ApiService")
+    //        .ReadFrom.Configuration(builder.Configuration)
+    //    );
 
-        // The options pattern uses classes to provide strongly typed access to groups of related settings.
-        builder.Services.AddOptions();
-        builder.AddConfig();
-        Config config = builder.GetConfig();
+    //    builder.Logging.AddAzureWebAppDiagnostics();
+    //    builder.Services.Configure<AzureFileLoggerOptions>(options =>
+    //    {
+    //        options.FileName = "logs-"; // Log file name prefix
+    //        options.FileSizeLimit = 50 * 1024 * 1024; // 50 MB
+    //        options.RetainedFileCountLimit = 5; // Keep last 5 log files
+    //    });
 
-        string connectionString = default!;
+    //    // The options pattern uses classes to provide strongly typed access to groups of related settings.
+    //    builder.Services.AddOptions();
+    //    builder.AddConfig();
+    //    Config config = builder.GetConfig();
 
-        if (IsAzure())
-        {
-            Console.WriteLine("Running in Azure.");
-            // Get connection string from Azure key vault.
-            connectionString = GetAzureKeyVaultValue(builder, "db-conn");
-        }
-        else
-        {
-            Console.WriteLine("Running locally.");
-            // Get connection string from appsettings.json.
-            connectionString = builder.Configuration.GetConnectionString("CountryServiceConnectionString")!;
-        }
+    //    string connectionString = default!;
 
-        // Add data access.
-        //    builder.AddDataAccess(config.DatabaseSystem, builder.Configuration.GetConnectionString(Constants.CountryServiceConnectionStringName)!);
-        builder.AddDataAccess(config.DatabaseSystem, connectionString);
+    //    if (IsAzure())
+    //    {
+    //        Console.WriteLine("Running in Azure.");
+    //        // Get connection string from Azure key vault.
+    //        connectionString = GetAzureKeyVaultValue(builder, "db-conn");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Running locally.");
+    //        // Get connection string from appsettings.json.
+    //        connectionString = builder.Configuration["ConnectionString"]!;
+    //    }
 
-        // Basic health probe. (See below for endpoint)
-        builder.Services.AddHealthChecks();
+    //    // Add data access.
+    //    //    builder.AddDataAccess(config.DatabaseSystem, builder.Configuration.GetConnectionString(Constants.CountryServiceConnectionStringName)!);
+    //    builder.AddDataAccess(config.DatabaseSystem, connectionString);
 
-        // Prometheus metrics.
-        builder.Services.AddOpenTelemetry()
-            .WithMetrics(builder =>
-            {
-                builder.AddPrometheusExporter();
-                builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
-            });
+    //    // Basic health probe. (See below for endpoint)
+    //    builder.Services.AddHealthChecks();
 
-        // Access HttpContext through the injected IHttpContextAccessor.
-        builder.Services.AddHttpContextAccessor();
+    //    // Prometheus metrics.
+    //    builder.Services.AddOpenTelemetry()
+    //        .WithMetrics(builder =>
+    //        {
+    //            builder.AddPrometheusExporter();
+    //            builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+    //        });
 
-        // Example: how to add header propagation to a named HTTP client.
-        // Uncomment use header propagation below to use this. (approx line 160
-        //builder.Services.AddHttpClient("planetsClient", c =>
-        //{
-        //    c.BaseAddress = new Uri("https://localhost:xxxx/");
-        //}).AddHeaderPropagation(options => options.Headers.Add("x-correlation-id"));
+    //    // Access HttpContext through the injected IHttpContextAccessor.
+    //    builder.Services.AddHttpContextAccessor();
 
-        // Problem details service.
-        builder.Services.AddProblemDetails(options =>
-            options.CustomizeProblemDetails = ctx =>
-            {
-                // You can add custom properties to the ProblemDetails instance.
-                //  ctx.ProblemDetails.Extensions.Add("requestId", ctx.HttpContext.TraceIdentifier);
-            });
-        builder.Services.AddExceptionHandler<ExceptionToProblemDetailsHandler>();
+    //    // Example: how to add header propagation to a named HTTP client.
+    //    // Uncomment use header propagation below to use this. (approx line 160
+    //    //builder.Services.AddHttpClient("planetsClient", c =>
+    //    //{
+    //    //    c.BaseAddress = new Uri("https://localhost:xxxx/");
+    //    //}).AddHeaderPropagation(options => options.Headers.Add("x-correlation-id"));
 
-        // Generate Open API documentation for the API.
-        builder.Services.AddEndpointsApiExplorer();
+    //    // Problem details service.
+    //    builder.Services.AddProblemDetails(options =>
+    //        options.CustomizeProblemDetails = ctx =>
+    //        {
+    //            // You can add custom properties to the ProblemDetails instance.
+    //            //  ctx.ProblemDetails.Extensions.Add("requestId", ctx.HttpContext.TraceIdentifier);
+    //        });
+    //    builder.Services.AddExceptionHandler<ExceptionToProblemDetailsHandler>();
 
-        // Add the Open API document generation services.
-        builder.Services.AddOpenApi();
+    //    // Generate Open API documentation for the API.
+    //    builder.Services.AddEndpointsApiExplorer();
 
-        // Add custom code.
-        builder.AddCode();
+    //    // Add the Open API document generation services.
+    //    builder.Services.AddOpenApi();
 
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy(name: allowAdminApp,
-                policy =>
-                {
-                    policy.AllowAnyOrigin() // WithOrigins("http://localhost:4200") // Implement ICorsPolicyProvider.
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                });
-        });
+    //    // Add custom code.
+    //    builder.AddCode();
 
-        builder.Services.AddControllers(options =>
-        {
-            // https://learn.microsoft.com/en-us/aspnet/core/web-api/jsonpatch?view=aspnetcore-9.0
-            options.InputFormatters.Insert(0, JsonPatchInputFormatter.GetJsonPatchInputFormatter());
+    //    builder.Services.AddCors(options =>
+    //    {
+    //        options.AddPolicy(name: allowAdminApp,
+    //            policy =>
+    //            {
+    //                policy.AllowAnyOrigin() // WithOrigins("http://localhost:4200") // Implement ICorsPolicyProvider.
+    //                    .AllowAnyMethod()
+    //                    .AllowAnyHeader();
+    //            });
+    //    });
 
-            // Example: how to add a filter to the request processing pipeline.
-            // options.Filters.Add<ProblemDetailsExceptionFilter>();
-        })
-        .AddJsonOptions(options =>
-        {
-            options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;           
-            options.JsonSerializerOptions.PropertyNamingPolicy=JsonNamingPolicy.CamelCase;
-        })
-        .ConfigureApiBehaviorOptions(options =>
-        {
-            options.InvalidModelStateResponseFactory = context =>
-            {
-                return new ModelStateToValidationProblemDetails(
-                    context.HttpContext.RequestServices.GetRequiredService<ILogger<ModelStateToValidationProblemDetails>>(), 
-                    context.HttpContext.RequestServices.GetRequiredService<IProblemDetailsCreator>());
-            };
-        }).AddNewtonsoftJson();
+    //    builder.Services.AddControllers(options =>
+    //    {
+    //        // https://learn.microsoft.com/en-us/aspnet/core/web-api/jsonpatch?view=aspnetcore-9.0
+    //        options.InputFormatters.Insert(0, JsonPatchInputFormatter.GetJsonPatchInputFormatter());
+
+    //        // Example: how to add a filter to the request processing pipeline.
+    //        // options.Filters.Add<ProblemDetailsExceptionFilter>();
+    //    })
+    //    .AddJsonOptions(options =>
+    //    {
+    //        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    //        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;           
+    //        options.JsonSerializerOptions.PropertyNamingPolicy=JsonNamingPolicy.CamelCase;
+    //    })
+    //    .ConfigureApiBehaviorOptions(options =>
+    //    {
+    //        options.InvalidModelStateResponseFactory = context =>
+    //        {
+    //            return new ModelStateToValidationProblemDetails(
+    //                context.HttpContext.RequestServices.GetRequiredService<ILogger<ModelStateToValidationProblemDetails>>(), 
+    //                context.HttpContext.RequestServices.GetRequiredService<IProblemDetailsCreator>());
+    //        };
+    //    }).AddNewtonsoftJson();
 
         WebApplication app = builder.Build();
 
-        // Example: how to add middleware into the request processing pipeline.       
-        // app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+    //    app.Logger.LogInformation("WebApplication Build().");
 
-        // Custom middleware added using an IApplicationBuilder extension.
-        app.UseCorrelationId();
 
-        // Map health check endpoint.
-        app.MapHealthChecks("/healthz");
+    //    // Example: how to add middleware into the request processing pipeline.       
+    //    // app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
-        // Adds a StatusCodePages middleware with a default response handler that checks for
-        // responses with status codes between 400 and 599 that do not have a body.
-        app.UseStatusCodePages();
+    //    // Custom middleware added using an IApplicationBuilder extension.
+    //    app.UseCorrelationId();
 
-        // Adds a middleware to the pipeline that will catch exceptions, log them, and re-execute
-        // the request in an alternate pipeline. The request will not be re-executed if the response has already started.
-        app.UseExceptionHandler();
+    //    // Map health check endpoint.
+    //    app.MapHealthChecks("/healthz");
 
-        // WARNING: Using OpenAPI and Scalar in ALL environments.
-        //    if (app.Environment.IsDevelopment())
-        //    {
+    //    // Adds a StatusCodePages middleware with a default response handler that checks for
+    //    // responses with status codes between 400 and 599 that do not have a body.
+    //    app.UseStatusCodePages();
 
-            // OpenAPI document generation.
-            app.MapOpenApi();
+    //    // Adds a middleware to the pipeline that will catch exceptions, log them, and re-execute
+    //    // the request in an alternate pipeline. The request will not be re-executed if the response has already started.
+    //    app.UseExceptionHandler();
 
-            // https://scalar.com/
-            // Endpoints
-            // /openapi/v1.json
-            // /scalar
-            app.MapScalarApiReference(options =>
-            {
-                options
-                    .WithTitle("Country Service")
-                    .WithTheme(ScalarTheme.Purple)
-                    .WithDefaultHttpClient(ScalarTarget.JavaScript, ScalarClient.Axios);
-            });
-    //    }
+    //    // WARNING: Using OpenAPI and Scalar in ALL environments.
+    //    //    if (app.Environment.IsDevelopment())
+    //    //    {
 
-        // Adds a middleware that collect headers to be propagated to a HttpClient
-        // E.g. correlation id.
-        // app.UseHeaderPropagation();
+    //        // OpenAPI document generation.
+    //        app.MapOpenApi();
 
-        // Cross-Origin Resource Sharing.
-        // Use the CORS policy, defined above. 
-        app.UseCors(allowAdminApp);
+    //        // https://scalar.com/
+    //        // Endpoints
+    //        // /openapi/v1.json
+    //        // /scalar
+    //        app.MapScalarApiReference(options =>
+    //        {
+    //            options
+    //                .WithTitle("Country Service")
+    //                .WithTheme(ScalarTheme.Purple)
+    //                .WithDefaultHttpClient(ScalarTarget.JavaScript, ScalarClient.Axios);
+    //        });
+    ////    }
 
-        // Routing middleware to match the URLs of incoming requests and map them to actions.
-        app.MapControllers();
+    //    // Adds a middleware that collect headers to be propagated to a HttpClient
+    //    // E.g. correlation id.
+    //    // app.UseHeaderPropagation();
 
-        // Prometheus for metrics collection.
-        app.MapPrometheusScrapingEndpoint();
+    //    // Cross-Origin Resource Sharing.
+    //    // Use the CORS policy, defined above. 
+    //    app.UseCors(allowAdminApp);
+
+    //    // Routing middleware to match the URLs of incoming requests and map them to actions.
+    //    app.MapControllers();
+
+    //    // Prometheus for metrics collection.
+    //    app.MapPrometheusScrapingEndpoint();
 
         app.Run();
     }
